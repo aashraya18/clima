921f998ce41637abd0fecefe699615cb82b6d0d8
Aashraya Singal <aashraya18@gmail.com>
2020-04-28 15:43:45 +0530

    Initial commit

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 50.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'NotoEmoji',
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Spartan MB',
+);
+
+const kConditionTextStyle = TextStyle(
+    fontSize: 40.0,
+    fontFamily: 'NotoEmoji'
+);
+
+const kTextFieldInputDecoration = InputDecoration(
+    filled: true,
+    fillColor: Colors.white,
+    icon: Icon(
+      Icons.location_city,
+      color: Colors.white,
+    ),
+    hintText: 'Enter City Name',
+    hintStyle: TextStyle(
+      color: Colors.grey,
+    ),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+      borderSide: BorderSide.none,
+    )
+
+);
